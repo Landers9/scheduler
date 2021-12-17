5a67a755523114be182e200e13a46fd7461c7a43
Landers9 <franckaigba4@gmail.com>
2021-12-17 12:43:10 +0100

    ok

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,10 @@
 class HomesController < ApplicationController
 
   def index
+    @teacherRespo = User.where(is_profrespo: true).count
+    @teachers = User.where(is_prof: true).count
+    @rooms = Salle.count
+    @materials = Material.count
     @plannings = Planning.all.order(id: :asc)
   end
 
