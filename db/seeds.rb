5f67ae9e21406f70bff13b0c52525a8731360195
Landers9 <franckaigba4@gmail.com>
2021-12-17 13:25:54 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
-User.create!(email: "abc@gmail.com", password: "azerty", firstname:"Landers", lastname:"Franck", telephone:"66882020")
+User.create!(email: "abc@gmail.com", password: "azerty", firstname:"Landers", lastname:"Franck", telephone:"66882020", is_prof:true, is_profrespo:true, admin:true)
+
+User.create!(email: "abcprof@gmail.com", password: "azerty", firstname:"Libes", lastname:"Eudes", telephone:"66782023", is_prof:true)
+
+User.create!(email: "abcetudiant@gmail.com", password: "azerty", firstname:"Marie", lastname:"Belle", telephone:"66889023")
 
 (1..5).each do |x|
+
     User.create!(email: "abc#{x}@gmail.com", password: "azerty#{x}", firstname:"Firstname#{x}", lastname:"Lastname#{x}", telephone:"6688202#{x}")
 
     Salle.create(name: "Salle#{x}")
